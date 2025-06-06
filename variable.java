70c09c5f6fdaae0bc393c82b0a0c5000a8b4c818
DZT711 <nguyensihuynsh711@gmail.com>
2025-06-06 18:26:14 +0700

    first commit

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args) {
+        int myNumber = 5;
+        String myString = "Hello";
+        boolean myBoolean = true;
+
+        System.out.println("Number: " + myNumber);
+        System.out.println("String: " + myString);
+        System.out.println("Boolean: " + myBoolean);
+    }
+}
